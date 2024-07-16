2c627ee23cf48052112c089bad4436b63106c07e
Zekeriya <Zekeriya@ZEKERIYA>
2024-07-16 11:52:21 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zekeriya Karabey");
+            Console.WriteLine("Patika Bootcamp'e katılma amacım iyi bir Fullstack Web Developer olabilmek.");
+        }
+    }
+}
